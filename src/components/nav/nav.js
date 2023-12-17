a587bf1f51170be5997b2bea52833cba850f8f13
InesFrederickx <ines.frederickx@student.ehb.be>
2023-12-17 23:16:48 +0100

    Reverted back to old navigation
    - Didn't like the look of using react-scroll

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,5 +1,5 @@
+// In Nav.js
 import React, { Component } from 'react';
-import { Link } from 'react-scroll';
 import './nav.css';
 import logo from '../../assets/logo.png';
 
@@ -10,13 +10,13 @@ export class Nav extends Component {
       <nav className="navbar">
         <div className='container'>
           <div className="nav-links">
-            <Link to="about" className={`nav-item ${activeSection === 'about' ? 'active' : ''}`} smooth={true} duration={500}>ABOUT</Link>
-            <Link to="projects" className={`nav-item ${activeSection === 'projects' ? 'active' : ''}`} smooth={true} duration={500}>PROJECTS</Link>
-            <Link to="contact" className={`nav-item ${activeSection === 'contact' ? 'active' : ''}`} smooth={true} duration={500}>CONTACT</Link>
+            <a href="#about" className={`nav-item ${activeSection === 'about' ? 'active' : ''}`}>ABOUT</a>
+            <a href="#projects" className={`nav-item ${activeSection === 'projects' ? 'active' : ''}`}>WORK</a>
+            <a href="#contact" className={`nav-item ${activeSection === 'contact' ? 'active' : ''}`}>CONTACT</a>
           </div>
-          <Link to="home" classID='nav-logo-link'>
+          <a href="#">
             <img src={logo} alt="Logo" className="nav-logo" />
-          </Link>
+          </a>
         </div>
       </nav>
     )
